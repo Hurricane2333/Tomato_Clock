bff927983770fc5168e11160d4f929e49e1bbadc
Hurricane2333 <1078481972@qq.com>
2024-07-07 21:15:21 +0800

    Delete redundant files.

--- TC_main/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_TC_main_FILE F:/Qt_Project/TC_main/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/TC_main.exe)
-set(__QT_DEPLOY_TARGET_TC_main_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_TC_main_RUNTIME_DLLS F:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;F:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;F:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
--- TC_main/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/CMakeFiles/TC_main_autogen_timestamp_deps.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for TC_main_autogen_timestamp_deps.
